b561c0af99b942de2d756a929a9ec4e76cd7e637
Modbder <Modbder@gmail.com>
2015-07-22 16:04:15 +0300

    Additions to tc's tools

--- java/tb/common/item/ItemThauminiteShears.java
+++ java/tb/common/item/ItemThauminiteShears.java
@@ -3,7 +3,8 @@ package tb.common.item;
 import thaumcraft.api.IRepairable;
 import net.minecraft.item.ItemShears;
 
-public class ItemThauminiteShears extends ItemShears implements IRepairable{
+public class ItemThauminiteShears extends ItemShears implements IRepairable
+{
 
 	public ItemThauminiteShears() {
 		super();
---
+++ java/tb/common/item/ItemVoidShears.java
@@ -0,0 +1,31 @@
+package tb.common.item;
+
+import thaumcraft.api.IRepairable;
+import thaumcraft.api.IWarpingGear;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.EnumRarity;
+import net.minecraft.item.ItemShears;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class ItemVoidShears extends ItemShears implements IRepairable,IWarpingGear{
+	
+	public EnumRarity getRarity(ItemStack itemstack)
+	{
+		return EnumRarity.uncommon;
+	}
+	
+	public void onUpdate(ItemStack stk, World w, Entity entity, int slot, boolean held)
+	{
+		super.onUpdate(stk, w, entity, slot, held);
+		if ((stk.isItemDamaged()) && (entity != null) && (entity.ticksExisted % 20 == 0) && ((entity instanceof EntityLivingBase)))
+			stk.damageItem(-1, (EntityLivingBase)entity);
+	}
+	
+	public int getWarp(ItemStack itemstack, EntityPlayer player)
+	{
+		return 1;
+	}
+}
