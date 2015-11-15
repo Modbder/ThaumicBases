4937f620805a26486e3ab6a74940fa7792c215a8
Modbder <Modbder@gmail.com>
2015-11-15 04:24:03 +0300

    [1.8]Just a generic class

---
+++ java/tb/common/item/ItemSeeds.java
@@ -0,0 +1,105 @@
+package tb.common.item;
+
+import DummyCore.Client.Icon;
+import DummyCore.Client.IconRegister;
+import DummyCore.Utils.IOldItem;
+import net.minecraft.block.Block;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
+import net.minecraftforge.common.IPlantable;
+
+public class ItemSeeds extends Item implements IPlantable, IOldItem
+{
+	public Block crops;
+    public Block soilBlockID;
+    
+    public ItemSeeds(Block crops, Block soil)
+    {
+        this.crops = crops;
+        this.soilBlockID = soil;
+        this.setCreativeTab(CreativeTabs.tabMaterials);
+    }
+
+    public boolean onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+        if (side != EnumFacing.UP)
+        {
+            return false;
+        }
+        else if (!playerIn.canPlayerEdit(pos.offset(side), side, stack))
+        {
+            return false;
+        }
+        else if (worldIn.getBlockState(pos).getBlock().canSustainPlant(worldIn, pos, EnumFacing.UP, this) && worldIn.isAirBlock(pos.up()))
+        {
+            worldIn.setBlockState(pos.up(), this.crops.getDefaultState());
+            --stack.stackSize;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    @Override
+    public net.minecraftforge.common.EnumPlantType getPlantType(net.minecraft.world.IBlockAccess world, BlockPos pos)
+    {
+        return this.crops == net.minecraft.init.Blocks.nether_wart ? net.minecraftforge.common.EnumPlantType.Nether : net.minecraftforge.common.EnumPlantType.Crop;
+    }
+
+    @Override
+    public net.minecraft.block.state.IBlockState getPlant(net.minecraft.world.IBlockAccess world, BlockPos pos)
+    {
+        return this.crops.getDefaultState();
+    }
+    
+    Icon icon;
+    String textureName;
+    
+	public ItemSeeds setTextureName(String s)
+	{
+		textureName = s;
+		return this;
+	}
+
+	@Override
+	public Icon getIconFromDamage(int meta) {
+		return icon;
+	}
+
+	@Override
+	public Icon getIconFromItemStack(ItemStack stk) {
+		return getIconFromDamage(stk.getMetadata());
+	}
+
+	@Override
+	public void registerIcons(IconRegister reg) {
+		icon = reg.registerItemIcon(textureName);
+	}
+
+	@Override
+	public int getRenderPasses(ItemStack stk) {
+		return 0;
+	}
+
+	@Override
+	public Icon getIconFromItemStackAndRenderPass(ItemStack stk, int pass) {
+		return getIconFromItemStack(stk);
+	}
+
+	@Override
+	public boolean recreateIcon(ItemStack stk) {
+		return false;
+	}
+
+	@Override
+	public boolean render3D(ItemStack stk) {
+		return false;
+	}
+}
